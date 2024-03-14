6c80ee57b8454a9642edb77a439e3bf947d85102
ZhatkinVyacheslav <zhatkins1009@mail.ru>
2024-03-14 20:13:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RegistrationPage from './RegistrationPage';
+
+// Рендерим компонент HelloWorld в DOM
+ReactDOM.render(<RegistrationPage />, document.getElementById('root'));
